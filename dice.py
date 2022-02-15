e0d732e8860ee5c58ae891478d51eb2b737d17c2
lelauch <atess.cemm@gmail.com>
2022-02-15 11:50:13 +0300

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,12 +1,16 @@
 import random
+
+name1 = str(input("1. Kullanıcı, adınızı giriniz: "))
+name2 = str(input("2. Kullanıcı, adınızı giriniz: "))
+
 key = 1
 while key == 1:
 
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
 
-    user1 = int(input("1. Kullanıcı tahmini: "))
-    user2 = int(input("2. Kullanıcı tahmini: "))
+    user1 = int(input(name1 + "'in tahmini: "))
+    user2 = int(input(name2 + "'in tahmini: "))
 
     total = dice1 + dice2
 
@@ -15,10 +19,10 @@ while key == 1:
 
 
     if odds1 > odds2:
-        print("Kullanıcı 1 Kazandı")
+        print("Kazanan: " + name1)
 
     elif odds2 > odds1:
-        print("Kullanıcı 2 Kazandı")
+        print("Kazanan" + name2)
 
     else:
         print("Kazanan Yok")
